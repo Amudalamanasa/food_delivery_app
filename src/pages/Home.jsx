ea16d938b311e4ae4a0beb7433325312fd20b9da
Amudalamanasa <137158179+Amudalamanasa@users.noreply.github.com>
2025-03-23 15:54:28 +0530

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1 align="center"> Welcome to the Food Delivery App</h1>
+        <img src="./swiggy.jpg" alt="" 
+        style={{width:"1300px",height:"auto"}}/>
+        
+    </div>
+  )
+}
+
+export default Home
